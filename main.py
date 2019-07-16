f71bd42295a918c9fa9a5abc7f197688711b91c7
Jeffrey Duda <jeff.duda@gmail.com>
2019-07-16 15:44:25 -0400

    ENH: close to workig

--- main.py
+++ main.py
@@ -41,7 +41,30 @@ class BayesNetForm(FlaskForm):
 #    def NumberOfStates():
 #        return len(self.states)
 
-class NaiveBayesNetwork():
+class NaiveBayesNetworkNode:
+
+    # Initializer
+    def __init__(self, name, states):
+        self.name = name
+        self.states = states
+        self.parent = ''
+        self.category = ''
+        self.menuStates = []
+        self.menuStates.append(name)
+        for s in self.states:
+            self.menuStates.append(name+":"+s)
+        self.value = ''
+        self.menuValue = ''
+        self.priors = np.ones(len(self.states))
+        self.priors = self.priors / len(self.priors)
+        self.probs = []
+
+    def set_parent(self, node):
+        self.parent = node.name
+
+
+
+class NaiveBayesNetwork:
 
     #def __init__(self, filename):
     #    self.filename = filename
@@ -51,38 +74,175 @@ class NaiveBayesNetwork():
         self.filename = ""
         self.name = "ExampleNetwork"
         self.categories = ["Signal", "Spatial", "Time", "Miscellaneous", "Clinical"];
+        self.nodes = []
+
+    def add_node(self, node):
+        # error checking?
+        self.nodes.append(node)
+
+    def has_node(self,name):
+        flag = False
+        for n in self.nodes:
+            if (flag==False):
+                if n.name == name:
+                    flag = True
+        return flag
+
+    def node_states(self, name):
+        flag = False
+        states = []
+        for n in self.nodes:
+            if (flag==False):
+                if n.name == name:
+                    flag = True
+                    states = n.states
+        return states
+
+    def set_node_state(self, name, state):
+        for n in self.nodes:
+            if n.name == name:
+                if state in n.states:
+                    n.value = state
+                    n.menuValue = n.name+':'+n.value
+                else:
+                    raise Exception("Invalid state set")
+
+    def clear_node_state(self, name):
+        for n in self.nodes:
+            if n.name == name:
+                n.value = ''
+                n.menuValue = ''
+
+    def set_node_probs(self, name, probs):
+        for n in self.nodes:
+            if n.name == name:
+                n.probs = probs
+
+    def get_node_priors(self, name):
+        for n in self.nodes:
+            if n.name == name:
+                return(n.priors)
+
+    def get_node_states(self, name):
+        for n in self.nodes:
+            if n.name == name:
+                return(n.states)
+
+    def get_node_category(self, name):
+        for n in self.nodes:
+            if n.name == name:
+                return(n.category)
+
+    def number_of_nodes_in_category(self,category):
+        count = 0
+        for n in self.nodes:
+            if n.category == category:
+                count = count + 1
+        return count
+
+    def nodes_in_category(self,category):
+        nodes = []
+        for n in self.nodes:
+            if n.category == category:
+                nodes.append(n)
+        return nodes
 
     def read_csv(self, network_file):
         self.filename = network_file
 
         diseases = []
+        diseasePrior = []
         nodeNames = []
+        nodeCategories = []
+        nodeStates = []
+
         self.categories = []
+        prob = []
 
         with open(self.filename) as csv_file:
             csv_reader =  csv.reader(csv_file, delimiter=',')
             line_count = 0
             for row in csv_reader:
                 if  line_count == 0:
-                    nodeStates = row[2:len(row)-1]
-                    print(nodeStates)
-                    for n in nodeStates:
-                        nodeParts = n.split(":")
-                        nodeName = nodeParts[0] + ":" + nodeParts[1]
-                        nodeNames.append(nodeName)
-                        self.categories.append(nodeParts[0])
-                   nodeNames = np.unique(nodeNames)
+                    allNodeStates = row[2:len(row)]
 
-                   self.categories = np.unique(self.categories)
+                    for n in allNodeStates:
+                        nodeParts = n.split(":")
+                        nodeCategories.append(nodeParts[0])
+                        nodeNames.append(nodeParts[1])
+                        nodeStates.append(nodeParts[2])
+
+                    for n in np.unique(nodeNames):
+                        states = []
+                        cat = ''
+                        for i in range(0,len(nodeNames)):
+                            if nodeNames[i] == n:
+                                states.append(nodeStates[i])
+                                cat = nodeCategories[i]
+                        node = NaiveBayesNetworkNode(n,states)
+                        node.category = cat
+                        node.parent = "Diagnosis"
+                        self.nodes.append(node)
 
                 else:
                     diseases.append(row[0])
+                    diseasePrior.append(row[1])
+                    rValues = row[2:]
+                    prob.append(rValues)
+
+
                 line_count += 1
 
-        print( np.unique(nodeNames))
+        prob = np.vstack(prob)
 
-        print(self.categories)
-        return diseases
+        for n in self.nodes:
+            nMat = np.empty((len(diseases), len(n.states) ))
+            for s in range(0, len(n.states)):
+                for i in range(0,len(nodeNames)):
+                    if nodeNames[i] == n.name and n.states[s] == nodeStates[i]:
+                        nMat[:,s] = prob[:,i]
+
+            self.set_node_probs( n.name, nMat )
+
+        dNode = NaiveBayesNetworkNode("Diagnosis", diseases)
+        dNode.category = "Diagnosis"
+        dNode.priors = diseasePrior
+        self.nodes.append(dNode)
+        self.categories = np.unique(nodeCategories)
+
+        for n in self.nodes:
+            st =  " ".join(str(x) for x in n.states)
+            print( n.category + ' - ' + n.name + ' - ' + str(n.states))
+            print(n.probs)
+
+    def get_diagnoses(self, isRadiologic):
+        mat = []
+        if not(isRadiologic):
+            mat.append(self.get_node_priors("Diagnosis"))
+        else:
+            unityPrior = []
+            dis = self.get_node_priors("Diagnosis")
+            for x in dis:
+                unityPrior.append(1.0/len(dis))
+            mat.append(unityPrior)
+
+        for n in self.nodes:
+            if (n.value != ''):
+                if not(isRadiologic):
+                    idx = n.states.index(n.value)
+                    mat.append( n.probs[:,idx])
+                elif (n.category != "Clinical"):
+                    print(n.category + n.name)
+                    idx = n.states.index(n.value)
+                    mat.append( n.probs[:,idx])
+
+        mat = np.vstack(mat)
+        mat = mat.astype('float')
+        mat = np.prod(mat, axis=0)
+        mat = mat/np.sum(mat)
+        sorted = np.argsort(mat)
+        sorted = sorted[::-1]
+        return sorted, mat
 
 
     def read_sheet(self, table_file, tableName):
@@ -150,57 +310,41 @@ Bootstrap(app)
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    # make keys a dictionary
-    keys = {"Disease":"Clear",
-      "Colour":"Clear"
-      }
 
-    colours = ['Colour', 'Colour:Red', 'Colour:Blue', 'Colour:Black', 'Colour:Orange']
-
-    form = BayesNetForm()
-    cv = request.form.get('colour')
-
-    keys['Colour'] = cv
+    print('index')
 
     network = NaiveBayesNetwork();
     #network.read_sheet("/Users/jtduda/pkg/aries-app/12-06-2018_BG_Bayesian_network_NaiveBayes.xlsx", "BGnetwork")
-    diseases = network.read_csv("/Users/jtduda/pkg/aries-app/example.csv")
-    print(diseases)
+    network.read_csv("/Users/jtduda/pkg/aries-app/BasalGangliaV2.csv")
+    features = request.form.to_dict()
+    for k in features.keys():
 
-    return render_template('index.html', colours=colours,keys=keys,form=form, network=network, diseases=diseases)
+        parts = features.get(k).split(":")
+        if ( len(parts) > 1 ):
+            print(k + parts[1])
+            network.set_node_state(k, parts[1] )
+        else:
+            network.clear_node_state(k)
 
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    cv = request.form.get('colour')
-    return(str(cv))
+    sorted, mat =  network.get_diagnoses(False)
+    radSorted, radMat = network.get_diagnoses(True)
 
-# [START form]
-@app.route('/form', methods=['GET'])
-def form():
-    colours = ['Red', 'Blue', 'Black', 'Orange']
-    return render_template('form.html', colours=colours)
-    #return render_template('form.html')
-# [END form]
+    dis = network.get_node_states("Diagnosis")
+    dx = []
+    dxRad = []
 
+    maxDx = 10
+    if len(sorted) < maxDx:
+        maxDx = len(sorted)
 
+    for i in range(maxDx):
+        dx.append( (dis[sorted[i]],mat[sorted[i]]) )
 
-# [START submitted]
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
+    for i in range(len(radSorted)):
+        dxRad.append( (dis[radSorted[i]],radMat[radSorted[i]]) )
 
-    # [END submitted]
-    # [START render_template]
-    return render_template(
-        'submitted_form.html',
-        name=name,
-        email=email,
-        site=site,
-        comments=comments)
-    # [END render_template]
+    return render_template('index.html', network=network, dx=dx, dxRad=dxRad, dxLength=len(dx))
+
 
 
 @app.errorhandler(500)
