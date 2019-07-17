3ed128f6f3203bf3fa9cf6a5f25bddd132819135
Jeffrey Duda <jeff.duda@gmail.com>
2019-07-17 18:20:39 -0400

    ENH: highlighting0

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ import logging
 
 # [START imports]
 from flask import Flask, render_template, request
+import flask
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField
@@ -45,93 +46,105 @@ class NaiveBayesNetworkNode:
 
     # Initializer
     def __init__(self, name, states):
+        #self.name = name.replace('_', ' ')
         self.name = name
         self.states = states
         self.parent = ''
         self.category = ''
         self.menuStates = []
-        self.menuStates.append(name)
+        self.menuStates.append(self.name)
         for s in self.states:
-            self.menuStates.append(name+":"+s)
+            self.menuStates.append(self.name+":"+s)
         self.value = ''
         self.menuValue = ''
         self.priors = np.ones(len(self.states))
         self.priors = self.priors / len(self.priors)
         self.probs = []
+        self.hasChoice = ''
+        self.sensitive = ''
+        self.style = ''
+
 
     def set_parent(self, node):
         self.parent = node.name
 
+    def is_state_selected(self, menuChoice):
+        if menuChoice == self.menuValue:
+            return 'selected'
+        return ''
+
 
 
 class NaiveBayesNetwork:
-
-    #def __init__(self, filename):
-    #    self.filename = filename
-       # read file and setup network
 
     def __init__(self):
         self.filename = ""
         self.name = "ExampleNetwork"
         self.categories = ["Signal", "Spatial", "Time", "Miscellaneous", "Clinical"];
         self.nodes = []
+        self.nodeMap = {}
 
     def add_node(self, node):
-        # error checking?
         self.nodes.append(node)
+        self.nodeMap[node.name] = len(self.nodes)-1
+        print( node.name + str( len(self.nodes)-1 ))
 
     def has_node(self,name):
-        flag = False
-        for n in self.nodes:
-            if (flag==False):
-                if n.name == name:
-                    flag = True
-        return flag
+        return self.nodeMap.has_key(name)
 
-    def node_states(self, name):
-        flag = False
-        states = []
-        for n in self.nodes:
-            if (flag==False):
-                if n.name == name:
-                    flag = True
-                    states = n.states
-        return states
-
-    def set_node_state(self, name, state):
-        for n in self.nodes:
-            if n.name == name:
-                if state in n.states:
-                    n.value = state
-                    n.menuValue = n.name+':'+n.value
-                else:
-                    raise Exception("Invalid state set")
-
-    def clear_node_state(self, name):
-        for n in self.nodes:
-            if n.name == name:
-                n.value = ''
-                n.menuValue = ''
-
-    def set_node_probs(self, name, probs):
-        for n in self.nodes:
-            if n.name == name:
-                n.probs = probs
-
-    def get_node_priors(self, name):
-        for n in self.nodes:
-            if n.name == name:
-                return(n.priors)
+    def get_node(self, name):
+        if self.has_node(name):
+            return self.nodes[ self.nodeMap[name] ]
+        return None
 
     def get_node_states(self, name):
+        if self.has_node(name):
+            return self.nodes[ self.nodeMap[name] ].states
+        return None
+
+    def set_node_state(self, name, state):
+        if self.has_node(name):
+            n = self.nodeMap[name]
+            if state in self.nodes[n].states:
+                self.nodes[n].value = state
+                self.nodes[n].menuValue = self.nodes[n].name + ':' + state
+                self.nodes[n].hasChoice = 'hasChoice'
+                self.nodes[n].sensitive = ''
+            else:
+                raise Exception("Invalid state set: " + state )
+
+    def set_node_states_by_result(self, index):
         for n in self.nodes:
-            if n.name == name:
-                return(n.states)
+            if n.name != 'Diagnosis':
+                vals = n.probs[index,:].tolist()
+                state = vals.index(max(vals))
+                self.set_node_state(n.name, n.states[state])
+
+    def clear_node_state(self, name):
+        if self.has_node(name):
+            n = self.nodeMap[name]
+            self.nodes[n].value = ''
+            self.nodes[n].menuValue = ''
+            self.nodes[n].hasChoice = ''
+        else:
+            raise Exception("Node does not exist: " + name )
+
+    def set_node_probs(self, name, probs):
+        if self.has_node(name):
+            n = self.nodeMap[name]
+            self.nodes[n].probs = probs
+        else:
+            raise Exception("Node does not exist: " + name )
+
+    def get_node_priors(self, name):
+        if self.has_node(name):
+            return( self.nodes[ self.nodeMap[name] ].priors)
+        return None
 
     def get_node_category(self, name):
-        for n in self.nodes:
-            if n.name == name:
-                return(n.category)
+        if self.has_node(name):
+            return( self.nodes[ self.nodeMap[name] ].category)
+        return None
 
     def number_of_nodes_in_category(self,category):
         count = 0
@@ -139,6 +152,13 @@ class NaiveBayesNetwork:
             if n.category == category:
                 count = count + 1
         return count
+
+    def names_of_nodes_in_category(self,category):
+        names = []
+        for n in self.nodes:
+            if n.category == category:
+                names.append(n.name)
+        return names
 
     def nodes_in_category(self,category):
         nodes = []
@@ -182,7 +202,7 @@ class NaiveBayesNetwork:
                         node = NaiveBayesNetworkNode(n,states)
                         node.category = cat
                         node.parent = "Diagnosis"
-                        self.nodes.append(node)
+                        self.add_node(node)
 
                 else:
                     diseases.append(row[0])
@@ -207,13 +227,32 @@ class NaiveBayesNetwork:
         dNode = NaiveBayesNetworkNode("Diagnosis", diseases)
         dNode.category = "Diagnosis"
         dNode.priors = diseasePrior
-        self.nodes.append(dNode)
+        self.add_node(dNode)
         self.categories = np.unique(nodeCategories)
 
-        for n in self.nodes:
-            st =  " ".join(str(x) for x in n.states)
-            print( n.category + ' - ' + n.name + ' - ' + str(n.states))
-            print(n.probs)
+    def calculate_node_sensitivity(self, name, currentProbs):
+
+        if ( not(self.has_node(name)) ):
+            return 0
+
+        node = self.nodes[ self.nodeMap[name] ]
+        val = node.value
+        probs = currentProbs*0
+
+        for v in range(len(node.states)):
+            self.set_node_state(node.name, node.states[v])
+            s, vProbs = self.get_diagnoses(False)
+            probs += vProbs / ( currentProbs * node.probs[:,v] )
+            probs[np.isnan(probs)] = 0
+
+        if val == '':
+            self.clear_node_state(node.name)
+        else:
+            self.set_node_state(node.name, val)
+
+        sens = probs.sum()
+        return sens
+
 
     def get_diagnoses(self, isRadiologic):
         mat = []
@@ -230,14 +269,18 @@ class NaiveBayesNetwork:
             if (n.value != ''):
                 if not(isRadiologic):
                     idx = n.states.index(n.value)
+                    #print(n.name + ' ' + str(idx) + ' ' + n.value)
+                    #print(n.probs)
+                    #print( n.probs[:,idx])
                     mat.append( n.probs[:,idx])
                 elif (n.category != "Clinical"):
-                    print(n.category + n.name)
+                    #print(n.category + n.name)
                     idx = n.states.index(n.value)
                     mat.append( n.probs[:,idx])
 
+
         mat = np.vstack(mat)
-        mat = mat.astype('float')
+        mat = mat.astype('double')
         mat = np.prod(mat, axis=0)
         mat = mat/np.sum(mat)
         sorted = np.argsort(mat)
@@ -302,34 +345,85 @@ class NaiveBayesNetwork:
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'aries-key'
+
+app.highlight = False
+app.highlightText = "Highlight most discriminating features"
+
+app.network = NaiveBayesNetwork();
+app.network.read_csv("/Users/jtduda/pkg/aries-app/BasalGangliaV2.csv")
+
 Bootstrap(app)
 # [END create_app]
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html', page='about')
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
 
-    print('index')
+    #print('index')
+    #print(request)
+    print(request.form)
 
-    network = NaiveBayesNetwork();
+    #network = NaiveBayesNetwork();
     #network.read_sheet("/Users/jtduda/pkg/aries-app/12-06-2018_BG_Bayesian_network_NaiveBayes.xlsx", "BGnetwork")
-    network.read_csv("/Users/jtduda/pkg/aries-app/BasalGangliaV2.csv")
-    features = request.form.to_dict()
-    for k in features.keys():
+    #network.read_csv("/Users/jtduda/pkg/aries-app/BasalGangliaV2.csv")
 
-        parts = features.get(k).split(":")
-        if ( len(parts) > 1 ):
-            print(k + parts[1])
-            network.set_node_state(k, parts[1] )
+    network = flask.current_app.network
+
+    dxList = network.get_node_states("Diagnosis")
+    setDx = ''
+
+    # For setting all features by diagnosis
+    features = request.form.to_dict()
+
+    #if ( request.method=='POST'):
+
+    if 'ClearDiagnosis' in features:
+        print("Clearing features")
+        setDx = ''
+        for n in network.nodes:
+            network.clear_node_state(n.name)
+
+    elif 'HighlightFeatures' in features:
+        print("Highlight features swap state = " + str(flask.current_app.highlight))
+        if flask.current_app.highlight == True:
+            flask.current_app.highlight = False;
+            flask.current_app.highlightText =  "Highlight most discriminating features"
+            for n in network.nodeMap.keys():
+                network.nodes[ network.nodeMap[n] ].sensitive = ''
         else:
-            network.clear_node_state(k)
+            flask.current_app.highlight = True;
+            flask.current_app.highlightText = "     Remove feature highlighting      "
+
+        print("Now set to " + str(flask.current_app.highlight))
+
+    elif 'SetDiagnosis' in features:
+        setDx = features.get("SetDiagnosis")
+        print( "Set features by diagnosis "+setDx)
+        if ( setDx=="cleardx"):
+            for n in network.nodes:
+                network.clear_node_state(n.name)
+            setDx = ''
+        else:
+            dxIndex = dxList.index( setDx )
+            network.set_node_states_by_result( dxIndex )
+
+    print('Feature selection')
+    for k in features.keys():
+        parts = features.get(k).split(":")
+        if network.has_node(k):
+            if ( len(parts) > 1 ):
+                network.set_node_state(k, parts[1] )
+            else:
+                network.clear_node_state(k)
 
     sorted, mat =  network.get_diagnoses(False)
     radSorted, radMat = network.get_diagnoses(True)
 
-    dis = network.get_node_states("Diagnosis")
     dx = []
     dxRad = []
 
@@ -337,13 +431,35 @@ def index():
     if len(sorted) < maxDx:
         maxDx = len(sorted)
 
+    cumSum = 0
     for i in range(maxDx):
-        dx.append( (dis[sorted[i]],mat[sorted[i]]) )
+        cumSum += mat[sorted[i]]
+        dx.append( (dxList[sorted[i]],mat[sorted[i]],cumSum) )
 
+    cumSum = 0
     for i in range(len(radSorted)):
-        dxRad.append( (dis[radSorted[i]],radMat[radSorted[i]]) )
+        cumSum += radMat[radSorted[i]]
+        dxRad.append( (dxList[radSorted[i]],radMat[radSorted[i]],cumSum) )
 
-    return render_template('index.html', network=network, dx=dx, dxRad=dxRad, dxLength=len(dx))
+    if flask.current_app.highlight:
+        print('Calculating sensitivities')
+        for cat in network.categories:
+            print(cat)
+            nodes = network.names_of_nodes_in_category(cat)
+            maxSens = 0
+            maxName = ''
+            for n in nodes:
+                network.nodes[ network.nodeMap[n] ].sensitive = ''
+                if network.nodes[ network.nodeMap[n] ].value == '':
+                    s = network.calculate_node_sensitivity(n, mat)
+                    print( n + ' ' + str(s))
+                    if s > maxSens:
+                        maxSens = s
+                        maxName = n
+            network.nodes[ network.nodeMap[maxName] ].sensitive = 'sensitive'
+
+
+    return render_template('index.html', network=network, dx=dx, dxRad=dxRad, dxLength=len(dx), page='index', setDx=setDx, highlight=flask.current_app.highlightText)
 
 
 
